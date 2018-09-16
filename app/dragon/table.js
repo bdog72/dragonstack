2e276c4eb27b86a7a1885a52c3e52609e8acf776
Brian Bycynski <high5bri@hotmail.com>
2018-09-16 09:17:28 -0400

    finished lecture 57, now starting the fronend with React

--- app/dragon/table.js
+++ app/dragon/table.js
@@ -1,5 +1,7 @@
 const pool = require('../../databasePool');
 
+const DragonTraitTable = require('../dragonTrait/table');
+
 class DragonTable {
   static storeDragon(dragon) {
     const { birthdate, nickname, generationId } = dragon;
@@ -13,7 +15,34 @@ class DragonTable {
 
           const dragonId = response.rows[0].id;
 
-          resolve({ dragonId });
+          Promise.all(
+            dragon.traits.map(({ traitType, traitValue }) => {
+              return DragonTraitTable.storeDragonTrait({
+                dragonId,
+                traitType,
+                traitValue
+              });
+            })
+          ).then(() => {
+            resolve({ dragonId }).catch(error => reject(error));
+          });
+        }
+      );
+    });
+  }
+  static getDragon({ dragonId }) {
+    return new Promise((resolve, reject) => {
+      pool.query(
+        `SELECT birthdate, nickname, "generationId" 
+         FROM dragon 
+         WHERE dragon.id = $1`,
+        [dragonId],
+        (error, response) => {
+          if (error) return reject(error);
+
+          if (response.rows.length === 0) return reject(new Error('no dragon'));
+
+          resolve(response.rows[0]);
         }
       );
     });
